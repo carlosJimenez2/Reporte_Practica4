14aea5f94ea62cb49b227af0a8919afe4c1cab09
carlosJimenez2 <42255224+carlosJimenez2@users.noreply.github.com>
2018-09-05 15:40:11 -0500

    Add files via upload

---
+++ Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaOperadores
+{
+    public class Persona
+    {
+        public double salarios;
+        public Persona(double salario)
+        {
+            salarios = salario;
+        }
+
+        public static double operator +(Persona x, Persona y)
+        {
+            return x.salarios + y.salarios;
+        }
+
+        
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SobrecargaDeMetodos
+{
+    class Program
+    {
+        public double prom(double cal1, double cal2)
+        {
+            double p = (cal1 + cal2) / 2;
+            return p;
+        }
+
+        public double prom(double cal1, double cal2, double pextra)
+        {
+            double p = (cal1 + cal2) / 2 + pextra;
+            return p;
+        }
+
+        static void Main(string[] args)
+        {
+            Program promedio = new Program();
+            Console.WriteLine("El promedio del alumno es = " + promedio.prom(6, 7.5));
+            Console.WriteLine("El promedio del alumno es = " + promedio.prom(5, 8,.6));
+            Console.ReadKey();
+        
+        }
+    }
+}
